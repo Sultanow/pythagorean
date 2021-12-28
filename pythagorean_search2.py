d737a09550763efc824a84569dbc80d21c4e3c10
Sultanow <sultanow@outlook.com>
2021-12-28 17:32:26 +0100

    Create pythagorean_search2.py

---
+++ pythagorean_search2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sys
+from z3 import Ints, IntVal, solve
+
+# for z3 consider:
+# https://stackoverflow.com/questions/61170977/z3-solver-installed-but-i-cant-import-anything
+# https://github.com/Z3Prover/z3/wiki/Using-Z3Py-on-Windows
+def main() -> int:
+    df = pd.read_csv('pythagorean_stu.txt', header=None)
+    
+    tuples = df.to_numpy()
+
+    x, y, z, w = Ints('x y z w')
+    for row in tuples:
+        s=int(row[3])
+        t=int(row[4])
+        u=int(row[5])
+        solve(x*x-w*w==s, y*y-w*w==t, z*z-y*y==u, w>0)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
